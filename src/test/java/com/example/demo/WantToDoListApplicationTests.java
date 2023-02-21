cd8c84ede957621de1711d07d1309d2647249b9a
SHUNSUKE-OGAWA <ogasyun0401@gmail.com>
2023-02-21 18:10:45 +0900

    initial commit

---
+++ src/test/java/com/example/demo/WantToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
