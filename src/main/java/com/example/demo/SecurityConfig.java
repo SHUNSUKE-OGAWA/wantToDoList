db232f1856e2f134befda5d3f70348b15ffc0023
SHUNSUKE-OGAWA <ogasyun0401@gmail.com>
2023-02-27 22:34:55 +0900

    簡易的なログイン機能を実装

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,17 +1,26 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
+
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+    private UserDetailsService userDetailsService; 
+	
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -19,22 +28,28 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
     
-
+    @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().requestMatchers("/css/**");
+        web.ignoring().antMatchers("/css/**");
     }
-
     
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    	//認証の設定を記述する
-    	http.formLogin(login -> login
-                .loginPage("/login")
-                .defaultSuccessUrl("/")
-                .permitAll()
-        ).authorizeHttpRequests(authz -> authz
-                .anyRequest().authenticated()
-        );
-    			
-        return http.build();
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest()
+            .authenticated();
+        http.formLogin()
+            .loginPage("/login")
+            .defaultSuccessUrl("/")
+            .permitAll();
+        http.logout()
+        	.permitAll();
+    }
+    
 }
