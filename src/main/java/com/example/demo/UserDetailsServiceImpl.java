04cfcb866bc8fb1c44c438723a0d66cc954d9a4e
SHUNSUKE-OGAWA <ogasyun0401@gmail.com>
2023-03-01 19:32:20 +0900

    ユーザ登録機能を追加

--- src/main/java/com/example/demo/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/UserDetailsServiceImpl.java
@@ -5,19 +5,26 @@ import java.util.Collection;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 	
 	@Autowired
 	JdbcTemplate jdbcTemplate;
+	
+	@Lazy
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -31,6 +38,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         } catch (Exception e) {
             throw new UsernameNotFoundException("user not found.", e);
         }
+    }
+	
+	@Transactional
+    public void register(String username, String password, String authority) {
+        String sql = "INSERT INTO user(name, password, authority) VALUES(?, ?, ?)";
+        jdbcTemplate.update(sql, username, passwordEncoder.encode(password), authority);
     }
 
 }
--- src/main/java/com/example/demo/app/AppController.java
+++ src/main/java/com/example/demo/app/AppController.java
@@ -1,12 +1,22 @@
 package com.example.demo.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.SignupForm;
+import com.example.demo.UserDetailsServiceImpl;
 
 @Controller
 @RequestMapping("/")
 public class AppController {
+	
+	@Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
 	
 	
 	@GetMapping()
@@ -17,6 +27,22 @@ public class AppController {
 	@GetMapping("/login")
     public String login() {
     	return "login";
+    }
+	
+	@GetMapping("/signup")
+    public String newSignup(SignupForm signupForm) {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(SignupForm signupForm, Model model) {
+        try {
+            userDetailsServiceImpl.register(signupForm.getUsername(), signupForm.getPassword(), "ROLE_USER");
+        } catch (DataAccessException e) {
+            model.addAttribute("signupError", "ユーザー登録に失敗しました");
+            return "signup";
+        }
+        return "redirect:/";
     }
 
 }
