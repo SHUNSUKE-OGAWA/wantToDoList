919cc419c3b3f6e0f88ae0f4a16d4e30430e8a08
SHUNSUKE-OGAWA <ogasyun0401@gmail.com>
2023-03-05 00:05:21 +0900

    Daoクラスを作成、Serviceクラスを変更

---
+++ src/main/java/com/example/demo/UserDetailsDao.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserDetailsDao {
+	
+	UserDetails findNameByUser(String username);
+	
+	void insertUser(String username, String password, String authority);
+	
+	int findUserCountByUser(String username);
+
+}
--- src/main/java/com/example/demo/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/UserDetailsServiceImpl.java
@@ -1,18 +1,9 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,21 +11,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserDetailsServiceImpl implements UserDetailsService {
 	
 	@Autowired
-	JdbcTemplate jdbcTemplate;
-	
-	@Lazy
-	@Autowired
-	PasswordEncoder passwordEncoder;
+	UserDetailsDao userDetailsDao;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		try {
-            String sql = "SELECT * FROM user WHERE name = ?";
-            Map<String, Object> map = jdbcTemplate.queryForMap(sql, username);
-            String password = (String)map.get("password");
-            Collection<GrantedAuthority> authorities = new ArrayList<>();
-            authorities.add(new SimpleGrantedAuthority((String)map.get("authority")));
-            return new UserDetailsImpl(username, password, authorities);
+            return userDetailsDao.findNameByUser(username);
         } catch (Exception e) {
             throw new UsernameNotFoundException("user not found.", e);
         }
@@ -42,14 +24,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	
 	@Transactional
     public void register(String username, String password, String authority) {
-        String sql = "INSERT INTO user(name, password, authority) VALUES(?, ?, ?)";
-        jdbcTemplate.update(sql, username, passwordEncoder.encode(password), authority);
+        userDetailsDao.insertUser(username, password, authority);
     }
 	
 	public boolean isExistUser(String username) {
-        String sql = "SELECT COUNT(*) FROM user WHERE name = ?";
-        int count = jdbcTemplate.queryForObject(sql, Integer.class, new Object[] { username });
-        if (count == 0) {
+        if (userDetailsDao.findUserCountByUser(username) == 0) {
             return false;
         }
         return true;
